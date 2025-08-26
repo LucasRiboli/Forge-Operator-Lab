9520b3947158e1db17e2115795b6e1a71581d5a8
lucas riboli <lucasriboli57@gmail.com>
2025-08-25 22:06:16 -0300

    feature: esboco do lab-init

--- lab-init.sh
+++ lab-init.sh
@@ -1,1 +1,11 @@
+# organizar init do lab que inicia o processo dos pods
+# simula o start do k8s kkkkk
+
 incus init images:ubuntu/22.04 pod --config limits.memory=1GB
+
+
+# stresse do pod, colocar em timer pra comecar
+
+incus exec pod -- apt update
+incus exec pod -- apt install -y stress-ng
+incus exec pod -- stress-ng --vm 1 --vm-bytes 50% --timeout 300s
